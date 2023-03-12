d0952217c1edf9aa1ee64536ba5dc13aea5e5bb4
Ert-Demir <123229651+Ert-Demir@users.noreply.github.com>
2023-03-12 11:30:34 +0100

    Add files via upload

---
+++ Virus Classes.js
@@ -0,0 +1,57 @@
+
+// ------------ Enemy Classes ------------- //
+
+  class Virus1 {
+    constructor (ap, hp, speed, x, y, width, height, image ){
+    this.x = canvas.width;
+    this.y = 0;
+    this.width = 100;
+    this.height = 100;
+    this.image = new Image();
+    this.image.src = 'images/lvl1.png';
+}
+}
+
+class Virus2 {
+    constructor (ap, hp, speed, x, y, width, height, image ){
+    this.x = canvas.width;
+    this.y = 0;
+    this.width = 100;
+    this.height = 100;
+    this.image = new Image();
+    this.image.src = 'images/lvl1.png';
+}
+}
+
+class Virus3 {
+    constructor (ap, hp, speed, x, y, width, height, image ){
+    this.x = canvas.width;
+    this.y = 0;
+    this.width = 100;
+    this.height = 100;
+    this.image = new Image();
+    this.image.src = 'images/lvl1.png';
+}
+}
+
+class Virus4 {
+    constructor (ap, hp, speed, x, y, width, height, image ){
+    this.x = canvas.width;
+    this.y = 0;
+    this.width = 100;
+    this.height = 100;
+    this.image = new Image();
+    this.image.src = 'images/lvl1.png';
+}
+}
+
+class Virus5 {
+    constructor (ap, hp, speed, x, y, width, height, image ){
+    this.x = canvas.width;
+    this.y = 0;
+    this.width = 100;
+    this.height = 100;
+    this.image = new Image();
+    this.image.src = 'images/lvl1.png';
+}
+}
